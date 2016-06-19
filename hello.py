e70f0bbafda5b6b855bb201f85ac916a99da1ecf
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-06-19 22:53:35 +0100

    ex of setting response cookie

--- hello.py
+++ hello.py
@@ -1,16 +1,23 @@
-from flask import Flask
+from flask import Flask, request ,make_response
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello world"
+    user_agent = request.headers.get('User-Agent')
+    return "Hello world, your browser is {}".format(user_agent)
 
 
-@app.route('/<name>')
+@app.route('/user/<name>')
 def user(name):
     return "Hello {}!".format(name)
+
+@app.route('/cookie')
+def response_cookie():
+    response = make_response('This doc carries cookie')
+    response.set_cookie('answer', '42')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
