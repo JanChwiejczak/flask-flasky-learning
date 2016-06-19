5a16f18f5cc012fa8f56bdaee20f5977d8d674ff
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-06-19 21:26:28 +0100

    Hello world app in flask

---
+++ hello.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello world"
+
+
+@app.route('/<name>')
+def user(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
