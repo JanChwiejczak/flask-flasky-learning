13bb56b6b24f730ba67b60ce50ba80dcf7d184e3
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-06-20 22:30:03 +0100

    Add flask bootstrap extension

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def agent():
