7b5ef3f1c03847d95d1c42f7eec238eaf008567d
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-06-20 23:03:51 +0100

    Add error handler routes & templates

--- hello.py
+++ hello.py
@@ -23,5 +23,13 @@ def response_cookie():
     response.set_cookie('answer', '42')
     return response
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error():
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
