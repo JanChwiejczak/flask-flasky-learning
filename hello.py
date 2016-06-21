cecb317889c7c5049266fc79d6c6d255e72fbd68
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-06-21 22:16:20 +0100

    add ex form style in bootstrap automatically!

--- hello.py
+++ hello.py
@@ -3,23 +3,36 @@ from datetime import datetime
 from flask import Flask, request, make_response, render_template, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "kljhk^%^;:jas76478tgfy;//z"
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 @app.route('/agent')
 def agent():
     user_agent = request.headers.get('User-Agent')
     return "Hello world, your browser is {}".format(user_agent)
 
-@app.route('/')
-def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+@app.route('/time')
+def index_time():
+    return render_template('index_time.html', current_time=datetime.utcnow())
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', name=name, form=form)
 
 @app.route('/cookie')
 def response_cookie():
