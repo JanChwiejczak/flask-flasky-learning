62c6b18a9f8d52bf26a9f1a88e56dc82f9d5781c
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-07-13 23:35:47 +0100

    add user registration, logic,forms and templates

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import Form
-from wtforms import StringField, SubmitField, PasswordField, BooleanField
-from wtforms.validators import Email, DataRequired, Length
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import Email, DataRequired, Length, Regexp, EqualTo
+from ..models import User
 
 
 class LoginForm(Form):
@@ -8,3 +9,24 @@ class LoginForm(Form):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log in')
+
+
+class RegistrationForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField('Username', validators=[
+                                        DataRequired(),
+                                        Length(1, 64),
+                                        Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                        'Usernames must have only letters, numbers dots or underscores')
+    ])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', 'Passwords must match.')])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data.first()):
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data.first()):
+            raise ValidationError('Username already in use.')
